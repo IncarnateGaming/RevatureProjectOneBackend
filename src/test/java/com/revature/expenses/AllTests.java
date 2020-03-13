ef122ca6d05b23bc85fe4424d23e812d2efb4cd2
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-03-13 15:18:18 -0400

    test suite

--- src/test/java/com/revature/expenses/AllTests.java
+++ src/test/java/com/revature/expenses/AllTests.java
@@ -3,8 +3,16 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.revature.expenses.services.helpers.LoggerSingletonTest;
+import com.revature.expenses.services.helpers.MathHelperTest;
+import com.revature.expenses.services.helpers.PasswordHelperTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({
+	LoggerSingletonTest.class,
+	MathHelperTest.class,
+	PasswordHelperTest.class
+})
 public class AllTests {
 
 }
--- src/test/java/com/revature/expenses/services/helpers/MathHelperTest.java
+++ src/test/java/com/revature/expenses/services/helpers/MathHelperTest.java
@@ -1,5 +1,38 @@
 package com.revature.expenses.services.helpers;
 
-public class MathHelperTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class MathHelperTest {
+	@Test
+	public void doubleOutTest1() {
+		String number = MathHelper.doubleTextOut(156.396421);
+		assertEquals("$156.39",number);
+	}
+	@Test
+	public void doubleOutTest2() {
+		String number = MathHelper.doubleTextOut(156_365_189.3336421);
+		assertEquals("$156,365,189.33",number);
+	}
+	@Test
+	public void doubleRound2_1() {
+		Double number = MathHelper.doubleRound2(156.396421);
+		assertEquals(156.40,number,.00001);
+	}
+	@Test
+	public void doubleRound2_2() {
+		Double number = MathHelper.doubleRound2(156_365_189.3336421);
+		assertEquals(156365189.33,number,.00000001);
+	}
+	@Test
+	public void doubleRound5_1() {
+		Double number = MathHelper.doubleRound5(156.396421);
+		assertEquals(156.39642,number,0.00000001);
+	}
+	@Test
+	public void doubleRound5_2() {
+		Double number = MathHelper.doubleRound5(156_365_189.3336461);
+		assertEquals(156365189.33365,number,.0000000001);
+	}
 }
